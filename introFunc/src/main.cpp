2fc6a1931da23bcd3c236935a7076e9ee8a80715
AndreyHP <andreymarins501@gmail.com>
2025-06-07 22:34:54 -0300

    introFunc

---
+++ introFunc/src/main.cpp
@@ -0,0 +1,10 @@
+#include "../include/add.hpp"
+#include <iostream>
+#include "../include/multi.hpp"
+
+
+int main(){
+    std::cout << add(10, 20) << '\n';
+    std::cout << multi(10,10) << '\n';
+    return 0;
+}
